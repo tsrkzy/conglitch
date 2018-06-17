2db36a892babb50b7cda5cbfe2e70b021450ddc8
tashiro <tsrmix@gmail.com>
2018-06-17 21:18:10 +0900

    imple: jpeg glitch

--- src/client.js
+++ src/client.js
@@ -9,6 +9,7 @@ import base64ToByteArray from './base64ToByteArray.js';
 import splitWith0xDA from './splitWith0xDA.js';
 import parseDataChunk from './parseDataChunk.js';
 import byteArrayToBase64 from './byteArrayToBase64.js';
+import corrupt from './corrupt.js';
 
 window.onload = () => {
   const canvas = document.getElementById('c');
@@ -18,36 +19,43 @@ window.onload = () => {
   image.onload = () => {
     ctx.drawImage(image, 0, 0);
     const base64Full = canvas.toDataURL('image/jpeg');
-    const bytes = base64ToByteArray(base64Full);
-    const [beforeDA, afterDA] = splitWith0xDA(bytes);
-    const [daArray, ffArray] = parseDataChunk(afterDA);
+    const iMax = 10;
+    const jMax = 10;
+    for (let i = 0; i < iMax; i++) {
+      for (let j = 0; j < jMax; j++) {
+        const iSkip = [i];
+        const jSkip = [j];
+        const option = { iSkip, jSkip };
 
-    const glitchedByteArray = beforeDA.concat(daArray);
+        const glitchedDataURL = glitch(base64Full, option);
 
-    for (let i = 0; i < ffArray.length; i++) {
-      let ff = ffArray[i];
-      for (let j = 0; j < ff.length; j++) {
-
-        /* flip order */
-        // const k = ff.length - 1 - j;
-        // let f = ff[k];
-
-        let f = ff[j];
-        glitchedByteArray.push(f);
+        const img = new Image();
+        img.src = glitchedDataURL;
+        img.width = 128;
+        img.onload = () => {
+          const body = document.body;
+          body.appendChild(img);
+        };
       }
     }
-    const glitchedDataURL = byteArrayToBase64(glitchedByteArray);
-
-    const img = new Image();
-    img.src = glitchedDataURL;
-    img.onload = () => {
-      const body = document.body;
-      body.appendChild(img);
-    };
   };
 };
 
 function d2h(decimal) {
   let hex = `0x${decimal.toString(16).toUpperCase()}`;
   return hex;
+}
+
+function glitch(dataUrl, option) {
+  const { iSkip = [], jSkip = [] } = option;
+  const bytes = base64ToByteArray(dataUrl);
+  const [beforeDA, afterDA] = splitWith0xDA(bytes);
+  const [daArray, ffArray] = parseDataChunk(afterDA);
+  const headerArray = beforeDA.concat(daArray);
+
+  const glitchedFFArray = corrupt(ffArray, iSkip, jSkip);
+  const glitchedArray = headerArray.concat(glitchedFFArray);
+  const glitchedDataURL = byteArrayToBase64(glitchedArray);
+
+  return glitchedDataURL;
 }
